9265a1512f3199241aa76f057bf3545e5816cdaf
Ващенко Артем Ігорович <ipz231_vai@student.ztu.edu.ua>
2024-08-02 22:15:19 +0300

    Added temporary solution to the token refreshing

--- Client/src/store/epics/userEpic.ts
+++ Client/src/store/epics/userEpic.ts
@@ -7,10 +7,20 @@ import { sendRequest } from './helpers/request';
 const userEpic = (action$: any) =>
   action$.pipe(
     ofType(getUsers.type),
-    mergeMap(() => from(sendRequest(`query { user { get_all { username, firstName, lastName, role, permissions } } }`))
+    mergeMap(() => from(sendRequest(`query { user { get_all { username, firstName, lastName, role, permissions } } }`)) // <------
     .pipe(
         mergeMap((data) => of(setUsers(data.data.user.get_all))),
-        catchError((error) => of(setError(error.message))))
+        catchError((error) => {
+                                          // shitty implementation:
+          switch(error.message) {
+            case "UNAUTHORIZED":          // one of the ways how to do this.
+              console.log(error.message); // query still needs to be called again while reusing big chunk of the code above.
+              sendRequest(`mutation { auth { refresh } }`).then(json => localStorage.setItem('authToken', json.data.auth.refresh));
+              break;
+          }
+          return of(setError(error.message))
+        })
+      )
     )
 );
 
